fdb2af4bcad2883dc9aa8bf508c0015b6a30ab93
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2026-07-25 17:00:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+const btn = document.querySelector("button");
+const form = document.querySelector(".form-group");
+const tbody = document.getElementById("book-list");
+btn.addEventListener("click", () =>{
+	alert(document.getElementById("title").value);
+	alert(document.getElementById("author").value);
+	alert(document.getElementById("isbn").value);
+})
+
+function book_details(book){
+	const tr = document.createElement("tr");
+	for(let key in book){
+		const cell = document.createElement("td");
+		cell.innerText = book[key];
+		tr.appendChild(cell);
+	}
+	const actions = document.createElement("td");
+    const deleteButton = document.createElement("button");
+	deleteButton.addEventListener("click", deleteButtonFunction);
+}
+
+function deleteButtonFunction(e){
+	let delete_tr = e.target.parentNode.parentNode;
+	delete_tr.remove();
+}
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	let book = {
+		title  : e.target.title.value,
+		author : e.target.author.value,
+		isbn : e.target.isbn.value,
+	}
+	book_details(book);
+})
